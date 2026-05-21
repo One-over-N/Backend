f29a0358dc37b71be19f2fe076cb78e649e079bd
SeoHyeon <shyeon16@ewha.ac.kr>
2026-05-22 02:09:01 +0900

    [Refactor] 시큐리티 필터 체인 제외 설정

--- src/main/java/com/oneovern/global/config/SecurityConfig.java
+++ src/main/java/com/oneovern/global/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.oneovern.global.security.exception.CustomAccessDenied;
 import com.oneovern.global.security.exception.CustomEntryPoint;
 import com.oneovern.global.security.filter.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,21 +26,11 @@ public class SecurityConfig {
     private final CustomAccessDenied customAccessDenied;
     private final JwtAuthFilter jwtAuthFilter;
 
-    private final String[] allowUrls={ //인증 없이 접근 가능 경로들
-            // Swagger 허용
-            "/swagger-ui/**",
-            "/swagger-resources/**",
-            "/v3/api-docs/**",
-            //로그인
-            "/auth/**"
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests->requests
-                        .requestMatchers(allowUrls).permitAll() //인증 없이 접근 가능 경로 지정
-                        .anyRequest().authenticated() //그 외 모든 요청 인증 요구
+                        .anyRequest().authenticated() //모든 요청 인증 요구
                 )
                 .formLogin(AbstractHttpConfigurer::disable) //폼 로그인 비활성화
                 .sessionManagement(session->session //세션 관리 비활성화
@@ -62,5 +54,25 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web->web.ignoring().requestMatchers(
+                // Swagger 허용
+                "/swagger-ui/**",
+                "/swagger-resources/**",
+                "/v3/api-docs/**",
+                //로그인
+                "/auth/login",
+                //회원가입
+                "/auth/signup"
+        );
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthFilter> tenantFilterRegisteration(JwtAuthFilter filter){
+        FilterRegistrationBean<JwtAuthFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setEnabled(false); //JwtAuthFilter 톰캣 전역 필터로 미등록
+        return registration;
+    }
 
 }
