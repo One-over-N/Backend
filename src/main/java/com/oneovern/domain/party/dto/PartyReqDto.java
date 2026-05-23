6257c1ae5dad1a1d8695becdc97330e82720bf72
na0302 <153806568+na0302@users.noreply.github.com>
2026-05-24 01:31:00 +0900

    feat:  PartyReqDto생성

--- src/main/java/com/oneovern/domain/party/dto/PartyReqDto.java
+++ src/main/java/com/oneovern/domain/party/dto/PartyReqDto.java
@@ -1,4 +1,13 @@
 package com.oneovern.domain.party.dto;
 
+import lombok.Getter;
+
 public class PartyReqDto {
+
+    @Getter
+    public static class ProcessApplicationRequest {
+
+        private String status;
+
+    }
 }
