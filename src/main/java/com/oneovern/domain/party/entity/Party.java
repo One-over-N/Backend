e8936e3bab03011e00481596aeaadf4f4918fdfe
na0302 <153806568+na0302@users.noreply.github.com>
2026-05-24 00:33:30 +0900

    feat: PartyResDto 생성

--- src/main/java/com/oneovern/domain/party/entity/Party.java
+++ src/main/java/com/oneovern/domain/party/entity/Party.java
@@ -1,4 +1,43 @@
 package com.oneovern.domain.party.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Party {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long partyId;
+
+    @Column(nullable = false)
+    private String partyName;
+
+    @Column(nullable = false)
+    private String ottAccountId;
+
+    @Column(nullable = false)
+    private String ottAccountPassword;
+
+    @Column(nullable = false)
+    private String bank;
+
+    @Column(nullable = false)
+    private String bankAccount;
+
+    @Column(nullable = false)
+    private String partyStatus;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime startedAt;
+
+    private Long ottPlanId;
+    private Long leaderId;
 }
--- src/main/java/com/oneovern/domain/party/entity/mapping/PartyMember.java
+++ src/main/java/com/oneovern/domain/party/entity/mapping/PartyMember.java
@@ -1,4 +1,27 @@
 package com.oneovern.domain.party.entity.mapping;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class PartyMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long partyMemberId;
+
+    @Column(nullable = false)
+    private Long partyId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
