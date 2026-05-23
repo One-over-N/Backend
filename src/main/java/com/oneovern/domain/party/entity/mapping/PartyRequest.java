6ce551be24129ad136407d7e8ca93cf6250b508a
na0302 <153806568+na0302@users.noreply.github.com>
2026-05-24 00:45:34 +0900

    feat: PartyRequest 생성

--- src/main/java/com/oneovern/domain/party/entity/mapping/PartyRequest.java
+++ src/main/java/com/oneovern/domain/party/entity/mapping/PartyRequest.java
@@ -1,4 +1,31 @@
 package com.oneovern.domain.party.entity.mapping;
 
-public class PartyRequest  {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PartyRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long partyRequestId;
+
+    @Column(nullable = false)
+    private Long partyId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime processedAt;
 }
