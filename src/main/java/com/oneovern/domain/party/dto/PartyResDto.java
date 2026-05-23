7da71601841c47b2ec10f24c7f21a139fff195e9
na0302 <153806568+na0302@users.noreply.github.com>
2026-05-24 00:00:42 +0900

    feat: PartyResDto 생성

--- src/main/java/com/oneovern/domain/party/dto/PartyResDto.java
+++ src/main/java/com/oneovern/domain/party/dto/PartyResDto.java
@@ -1,4 +1,34 @@
 package com.oneovern.domain.party.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
 public class PartyResDto {
+
+    @Getter
+    @Builder
+    public static class JoinedPartyListResponse {
+        private List<JoinedPartyResponse> partyList;
+        private Integer totalCount;
+    }
+
+    @Getter
+    @Builder
+    public static class JoinedPartyResponse {
+        private Long partyId;
+        private String ottAccountID;
+        private String ottAccountPW;
+        private String bank;
+        private String bankAccount;
+        private PlanResponse plan;
+    }
+
+    @Getter
+    @Builder
+    public static class PlanResponse {
+        private Long planId;
+        private String planName;
+    }
 }
