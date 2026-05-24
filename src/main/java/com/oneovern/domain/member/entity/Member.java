333c69494a596020623fb2408ed6bd42b36e4fc1
SeoHyeon <shyeon16@ewha.ac.kr>
2026-05-24 13:43:11 +0900

    [Fix] member엔티티 id 컬럼 매핑

--- src/main/java/com/oneovern/domain/member/entity/Member.java
+++ src/main/java/com/oneovern/domain/member/entity/Member.java
@@ -25,6 +25,7 @@ public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
     @Column(name = "email", nullable = false, unique = true, length = 100)
