eb154f81f2e78c1dc7f773e52be4da96474f1bc2
SeoHyeon <shyeon16@ewha.ac.kr>
2026-05-25 20:34:14 +0900

    [Refactor] 리뷰 피드백 반영
    - SettlementSuccessCode 메시지 형식 통일
    
    - LocalDate.now를 clock을 주입받도록 수정
    - size를 application.yml에서 받아오도록 수정
    - dDay의 값이 없을 경우 null처리
    - Projection이 수정 불가능한 list일 경우를 대비하여 ArrayList로 감쌈

--- src/main/java/com/oneovern/domain/settlement/exception/code/SettlementSuccessCode.java
+++ src/main/java/com/oneovern/domain/settlement/exception/code/SettlementSuccessCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum SettlementSuccessCode implements BaseSuccessCode {
-    GET_CURRENT_MEMBER_PAYMENTS(HttpStatus.OK, "SETTLEMENT200_1", "성공적으로 이달 정산 현황이 조회되었습니다."),
-    GET_MEMBER_PAYMENT_SUMMARY(HttpStatus.OK, "SETTLEMENT200_2", "이번 달 정산 현황 요약을 성공적으로 불러왔습니다.");
+    GET_CURRENT_MEMBER_PAYMENTS(HttpStatus.OK, "SETTLEMENT200_1", "성공적으로 이번 달 정산 현황이 조회되었습니다."),
+    GET_MEMBER_PAYMENT_SUMMARY(HttpStatus.OK, "SETTLEMENT200_2", "성공적으로 이번 달 정산 현황 요약이 조회되었습니다.");
 
     private final HttpStatus status;
     private final String code;
