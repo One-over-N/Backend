9b7d9717bc7a9fb5dc02f726fdaaacb3d856c51d
SeoHyeon <shyeon16@ewha.ac.kr>
2026-06-08 02:35:16 +0900

    Revert "[Fix] 파티 가입시 트리거와 중복 로직 제거"

--- src/main/java/com/oneovern/domain/party/service/PartyService.java
+++ src/main/java/com/oneovern/domain/party/service/PartyService.java
@@ -145,27 +145,16 @@ public class PartyService {
             if (currentCount >= party.getOttPlan().getMaxMembers()) {
                 throw new PartyException(PartyErrorCode.PARTY_FULL);
             }
-        }
 
-        partyRepository.updateJoinRequestStatusNative(requestId, status.name());
-
-        if (isApproved) {
             partyRepository.savePartyMemberNative(partyId, applicantId);
 
             int newCount = partyRepository.getCurrentMemberCountNative(partyId);
             if (newCount >= party.getOttPlan().getMaxMembers()) {
-                LocalDate targetDate = LocalDate.now().withDayOfMonth(1);
-
-                partySettlementRepository.save(
-                        PartySettlement.builder()
-                                .party(party)
-                                .targetDate(targetDate)
-                                .targetAmount(party.getOttPlan().getMonthlyPrice())
-                                .settlementStatus(SettlementStatus.PENDING)
-                                .build()
-                );
+                partyRepository.updatePartyStatusNative(partyId, "CLOSED");
             }
         }
+
+        partyRepository.updateJoinRequestStatusNative(requestId, status.name());
 
         notificationRepository.save(Notification.builder()
                 .notificationType(isApproved ? NotificationType.JOIN_APPROVED : NotificationType.JOIN_REJECTED)
