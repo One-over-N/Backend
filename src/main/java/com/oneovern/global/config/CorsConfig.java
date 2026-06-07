5de482f527f209c0e2bbee9635a1e479483df544
kakao3838 <nandakora110@gmail.com>
2026-06-07 18:15:09 +0900

    fix: CorsConfig Vercel 주소 추가

--- src/main/java/com/oneovern/global/config/CorsConfig.java
+++ src/main/java/com/oneovern/global/config/CorsConfig.java
@@ -12,6 +12,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://frontend-ebon-five-27.vercel.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
